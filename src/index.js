79f50c171801299b2c491a0189f38765714f1126
Kim Seungha <seungha.me@gmail.com>
2018-10-17 16:12:41 +0900

    빙고 게임 완성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,110 @@
-import './index.scss';
+let boardState = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+]
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+function drawBoard() {
+  document.querySelectorAll('.row').forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll('.col').forEach((colEl, colIndex) => {
+      if (boardState[rowIndex][colIndex] === 1) {
+        colEl.classList.add('checked')
+      } else {
+        colEl.classList.remove('checked')
+      }
+    })
+  })
+
+  if (bingo(boardState)) {
+    document.querySelector('.result').textContent = '빙고!'
+    document.querySelector('.reset').classList.add('show')
+  } else {
+    document.querySelector('.result').textContent = ''
+    document.querySelector('.reset').classList.remove('show')
+  }
+}
+
+function bingo(arr) {
+  // 가로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true
+    for (let j = 0; j < 5; j++) {
+      if (arr[i][j] === 0) {
+        checked = false
+      }
+    }
+    if (checked) {
+      return true
+    }
+  }
+
+  // 세로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][i] === 0) {
+        checked = false
+      }
+    }
+    if (checked) {
+      return true
+    }
+  }
+
+  {
+    // 대각선 확인 (루프)
+    let checked = true
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][j] === 0) {
+        checked = false
+      }
+    }
+    if (checked) {
+      return true
+    }
+  }
+
+  {
+    // 반대쪽 대각선 확인 (루프)
+    let checked = true
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][4 - j] === 0) {
+        checked = false
+      }
+    }
+    if (checked) {
+      return true
+    }
+  }
+
+  return false
+}
+
+document.querySelectorAll('.row').forEach((rowEl, rowIndex) => {
+  rowEl.querySelectorAll('.col').forEach((colEl, colIndex) => {
+    colEl.addEventListener('click', e => {
+      if (!bingo(boardState)) {
+        boardState[rowIndex][colIndex] = 1
+        drawBoard()
+      }
+    })
+  })
+})
+
+document.querySelector('.reset').addEventListener('click', e => {
+  // alert('asdf')
+  boardState = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ]
+  drawBoard()
+})
+
+drawBoard()
